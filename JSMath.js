c76433b99dd29aa94cd078e8055a6eb1a4eba65b
Crasheldark <ertiojulito@gmail.com>
2025-04-05 20:37:52 +0200

    Numero Pi

--- JSMath.js
+++ JSMath.js
@@ -1,3 +1,6 @@
 //Genera un número aleatorio entre 0 y 100 redondeando y muéstralo por consola
 let numAleatorio1 = Math.round(Math.random() * 100);
 console.log(numAleatorio1);
+
+//Muestra por consola el valor de PI.
+console.log(Math.PI);
